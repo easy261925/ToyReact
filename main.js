c479124eccafd3862ea410fe333e1ab5f5efbf5e
cc <116012411@qq.com>
2020-08-03 21:10:10 +0800

    1st lesson

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-import { ToyReact } from './TyoReact'
+import { ToyReact, Component } from './TyoReact'
 
-class MyComponent {
-
+class MyComponent extends Component {
+  render() {
+    return <div>
+      <span>hello</span>
+      <span>react</span>
+      <div>
+        {this.children}
+      </div>
+    </div>
+  }
 }
 
-let a = <MyComponent name='cc' />
-console.log('main123')
+let a = <MyComponent name='b' id='idb'>
+  <span>Hello</span>
+  <span>MyComponent</span>
+  <span>!</span>
+</MyComponent>
+
+// document.body.appendChild(a)
+
+ToyReact.render(a, document.body)
